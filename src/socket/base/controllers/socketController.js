f132bfbfec70a2b51897df6bd8f66c22396891f7
beeget <yaman.ss1@hotmail.com>
2022-05-08 16:21:28 +0200

    New auto-reconnect system

--- src/socket/base/controllers/socketController.js
+++ src/socket/base/controllers/socketController.js
@@ -4,6 +4,7 @@ const ActionsTransfer = require('../transfer/actionsTransfer');
 
 class SocketController {
     static socket = null;
+    static connectingTries = 0;
     static startup(url, key, fxserver_type, version) {
         SocketController.startup_exec_timestamp = new Date();
         const socket = io(url, {
@@ -14,8 +15,7 @@ class SocketController {
                 transports: ["websocket"]
             },
 
-            'reconnectionDelay': 30000,
-            'reconnectionAttempts': 15
+            reconnection: false
         });
 
         SocketController.socket = socket;
@@ -24,16 +24,26 @@ class SocketController {
 
     static handler(socket) {
         socket.on('connect_error', (e) => {
+            SocketController.connectingTries++;
             console.log('\x1b[31m' + '[Socket] There was an error while connecting to Node, error : ' + e.message + '\x1b[0m')
+            if (SocketController.connectingTries <= 864) {
+                setTimeout(() => {
+                    socket.connect();
+                }, 30000 * (SocketController.connectingTries >= 10 ? 10 : SocketController.connectingTries))
+            }
         })
 
         socket.on("connect", () => {
             console.log('\x1b[32m' + '[Socket] Connector has been connected successfully to node. ' + (SocketController.startup_exec_timestamp != null ? '(' + ((new Date()) - SocketController.startup_exec_timestamp) + 'ms)' : '') + '\x1b[0m')
+            SocketController.connectingTries = 0;
             SocketController.startup_exec_timestamp = null;
         });
 
         socket.on("disconnect", (e) => {
             console.log('\x1b[31m' + '[Socket] Connector has been disconnected from node. reason : ' + e + '\x1b[0m')
+            setTimeout(() => {
+                socket.connect();
+            }, 30000)
         });
 
         socket.on("connector_up", (connector_id) => {
